0de729f22a8b277d785e7955c4461408753fce63
Darkon96 <steponframe2@gmail.com>
2024-03-18 01:37:04 +0300

    123

--- src/components/priceBlock.jsx
+++ src/components/priceBlock.jsx
@@ -1,12 +1,8 @@
-import rec from '../assets/rectangle_1.png';
-
 export default function Price(props) {
     const { price } = props;
     const { text } = props;
     return(
-        <div className="w-1/5 rounded-3xl price-block flex justify-items-center flex-wrap" style={{
-            'backgroundImage': `url(${rec.src})`,
-        }}>
+        <div className="w-1/5 rounded-3xl price-block flex justify-items-center flex-wrap bg-zinc-500">
             <p className="price-block-text-bold pt-7 w-full">{text}</p>
             <p className="price-block-text-regular w-full">{price} ₽/час *</p>
         </div>
