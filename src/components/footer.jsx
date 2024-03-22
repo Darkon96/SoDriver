203b8becc4ff58c46509a8c719c57a1eb52fa355
Darkon96 <steponframe2@gmail.com>
2024-03-22 11:33:04 +0300

    mobileLite

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,16 +1,13 @@
 const Footer = () => {
     return (
-        <footer className="footer bg-black grid grid-cols-12 grid-rows-3 gap-x-8 pt-3">
-            <div className="col-span-3 text-end bot-bold-text h-min">
-                <div className="row-span-1">Круглосуточно 24/7</div>
+        <footer className="footer bg-black">
+            <div className="pt-3">
+                <a href="tel:+79096298188" className="a-color text-regular h-min">+7 (909) 629-81-88</a>
             </div>
-                <a href="tel:+79096298188" className="a-color col-start-1 col-span-3 row-span-1 row-start-2 text-end h-min">+7 (909) 629-81-88</a>
-                <div><a href="mailto:Tvojvoditel@gmail.com" className="a-color h-min">Tvojvoditel@gmail.com</a></div>
-                <div className="col-span-4 col-start-4 h-min">Адрес: г. Москва</div>
-            <div className="row-span-1 row-start-3 col-span-12 text-center">
-                <div>Компания Мой Водитель © 2023</div>
-                <p className="bot-company-text text-xs">Авторские права: информация для правообладателей.По всем вопросам о возможном нарушение обращаться для взаимодействия на Tvojvoditel@gmail.com</p>
-            </div>
+            <div className="bot-bold-text h-min ">Круглосуточно 24/7</div>
+            <div><a href="mailto:Tvojvoditel@gmail.com" className="a-color h-min">Tvojvoditel@gmail.com</a></div>
+            <div className="text-center mt-3">Компания Мой Водитель © 2023</div>
+            <p className="bot-company-text text-xs mb-0 text-left">Авторские права: информация для правообладателей.По всем вопросам о возможном нарушение обращаться для взаимодействия на Tvojvoditel@gmail.com</p>
         </footer>
     );
 }
